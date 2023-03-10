91db65c5bca85608343299064adc20f4853f2e4d
Raul De Maio <rauldemaio@gmail.com>
2023-03-10 14:18:48 +0100

    update utils.py with new functions

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from pandas_datareader import data as pdr
 from datetime import date as dt
+import warnings
+
 
 import yfinance as yf
 yf.pdr_override()
@@ -11,6 +13,7 @@ PARAMS = {}
 PARAMS['EUNA.F']={'changepoint_prior_scale': 0.007, 'seasonality_prior_scale': 10.0, 'interval_width': 0.95}
 PARAMS['LCWD.MI']={'changepoint_prior_scale': 0.05, 'seasonality_prior_scale': 0.01, 'interval_width': 0.95}
 PARAMS['VWCE.MI']={'changepoint_prior_scale': 0.05, 'seasonality_prior_scale': 0.01, 'interval_width': 0.99}
+
 
 def ExtractData(list_of_tickers):
     data = {}
@@ -23,8 +26,64 @@ def ExtractData(list_of_tickers):
     return df
 
 def LoadStoredData():
-    
-    dfs = pd.read_parquet('../data/tickers_source.parquet')
+    '''
+    Load source file with ETF data and returns a groupedby DataFrame
+    '''
+    dfs = pd.read_parquet('data/tickers_source.parquet')
     dfs.drop(columns='Adj Close', inplace=True)
     dfs = dfs.groupby('ticker')
     return dfs
+
+def TransformaData2Prophet(df: pd.DataFrame):
+    '''Transform the input DataFrame and given in output a DataFrame which can be used for Prophet Forecasting.'''
+    df['Span'] = df.Close - df.Open 
+    df['Range'] = df.High - df.Low 
+    df['Night Span'] = df.Open - df.Close.shift(1)
+
+    df.dropna(inplace=True)
+    df.drop(columns=['Open','High','Low','Volume'], inplace=True)
+
+    data = df.copy()
+    data['ds'] = data.index
+    data['y'] = data['Close']
+    data = data[['ds','y']]
+
+    return data
+
+def TrainProphetModel(data: pd.DataFrame, ticker: str, periods: 30*6):
+    from prophet import Prophet
+
+    m = Prophet(**PARAMS[ticker]) 
+
+    m.add_country_holidays(country_name='US')
+    m.add_country_holidays(country_name='IT')
+
+    m.fit(data)
+
+    future = m.make_future_dataframe(periods=periods)
+    future = future[future.ds.dt.day_of_week<5]
+    forecast = m.predict(future)
+
+    return m, forecast
+
+def PlotResults(m, forecast,ticker):
+    from prophet import Prophet
+    import matplotlib.pyplot as mp
+    from prophet.plot import plot_plotly, plot_components_plotly
+    from prophet.plot import add_changepoints_to_plot
+    
+    fig1 = plot_plotly(m,forecast, ylabel=ticker)
+    fig3 = m.plot(forecast, ylabel = ticker)
+    fig1.show()
+    a = add_changepoints_to_plot(fig3.gca(), m, forecast)
+    fig2 = plot_components_plotly(m,forecast)
+    fig2.show()
+
+def ModelPerformance(m):
+
+    from prophet.diagnostics import cross_validation, performance_metrics
+    from prophet.plot import plot_cross_validation_metric
+
+    data_cv = cross_validation(m, horizon = '90 days', parallel='processes')
+    data_p = performance_metrics(data_cv)
+    return data_p
